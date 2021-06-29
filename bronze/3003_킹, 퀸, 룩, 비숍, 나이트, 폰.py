eec349a92dfb096e85c1ceeebcebf7a7f90e87d2
KSH_3710 <shockim3710@naver.com>
2021-06-29 19:40:24 +0900

    Create 3003_킹, 퀸, 룩, 비숍, 나이트, 폰.py
    백준 3003_킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ bronze/3003_킹, 퀸, 룩, 비숍, 나이트, 폰.py
@@ -0,0 +1,9 @@
+num = input()
+k = int(num.split(" ")[0])
+q = int(num.split(" ")[1])
+r = int(num.split(" ")[2])
+b = int(num.split(" ")[3])
+n = int(num.split(" ")[4])
+p = int(num.split(" ")[5])
+
+print(1-k, 1-q, 2-r, 2-b, 2-n, 8-p)
