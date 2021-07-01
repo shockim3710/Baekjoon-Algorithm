e4e3ae50eafdb0f425f312e39e364ab3a77ed0f6
KSH_3710 <shockim3710@naver.com>
2021-07-01 22:48:41 +0900

    Create 1547_공.py
    백준 1547_공

---
+++ bronze/1547_공.py
@@ -0,0 +1,12 @@
+M = int(input())
+result = 1
+
+for i in range(M):
+    X, Y = map(int, input().split())
+
+    if X == result:
+        result = Y
+    elif Y == result:
+        result = X
+
+print(result)
